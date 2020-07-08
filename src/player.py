dfa92a280e27f53547e44e2ca086949d1f4d8590
JWNicholson <john.nicholson812@gmail.com>
2020-07-08 16:26:42 -0400

    wrote pseudo code for player.py and room.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# class Player
+#constructor - self, name, current_location
+#return player name and current_location
--- src/room.py
+++ src/room.py
@@ -1,2 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# class Room
+# constructor self, name, description
+# set n, e, s, w to null
+#return name and description
