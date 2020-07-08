25c430b8b1af0796578508840d0cd6c731ed6760
JWNicholson <john.nicholson812@gmail.com>
2020-07-08 17:12:08 -0400

    Player class created in player.py

--- src/player.py
+++ src/player.py
@@ -4,3 +4,10 @@
 # class Player
 #constructor - self, name, current_location
 #return player name and current_location
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __repr__(self):
+        return f'Name: {self.name}, Current Room: {self.current_room}' 
