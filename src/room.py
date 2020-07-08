46c4e2952a8fbbb533a807890f4a41179aa6418f
JWNicholson <john.nicholson812@gmail.com>
2020-07-08 17:19:49 -0400

    Room class created in room.py

--- src/room.py
+++ src/room.py
@@ -5,3 +5,15 @@
 # constructor self, name, description
 # set n, e, s, w to null
 #return name and description
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __repr__(self):
+        return f'Name: {self.name}, Description: {self.description}'
